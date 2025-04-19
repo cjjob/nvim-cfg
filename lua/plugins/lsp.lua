ef3a4c3c4f9f644d16367dd6351793d2921d612a
Conor O'Brien <obriencjj@gmail.com>
2025-04-19 04:14:51 +0100

    Enable Rust LSP support.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -189,7 +189,7 @@ local lsp = {
                 -- clangd = {},
                 -- gopls = {},
                 pyright = {},
-                -- rust_analyzer = {},
+                rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
                 -- Some languages (like typescript) have entire language plugins that can be useful:
