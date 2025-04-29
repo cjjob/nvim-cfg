96bf5be21e870d69a0071056830fad6b3e4bc5da
Conor O'Brien <obriencjj@gmail.com>
2025-04-29 04:26:06 +0100

    Differentiate toggled terminal from regular buffer colours.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,1 +1,17 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+        hide_numbers = false,
+        highlights = {
+            NormalFloat = { guibg = "#000000" },
+            FloatBorder = {
+                guibg = "#000000",
+                guifg = "#00ff00", -- The border.
+            },
+        },
+        float_opts = {
+            border = "single",
+        },
+    },
+}
