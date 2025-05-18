67d0a872669fa4b75dd9f42223d5af813ae38cd6
Conor O'Brien <obriencjj@gmail.com>
2025-05-18 20:04:38 +0100

    Enable Go LSP.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -251,18 +251,7 @@ local lsp = {
                     filetypes = { "sh", "zsh" },
                 },
                 -- clangd = {},
-                -- gopls = {},
-                pyright = {},
-                rust_analyzer = {},
-                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                --
-                -- Some languages (like typescript) have entire language plugins that can be useful:
-                --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`ts_ls`) will work just fine
-                -- ts_ls = {},
-                --
-
+                gopls = {},
                 lua_ls = {
                     -- cmd = { ... },
                     -- filetypes = { ... },
@@ -277,6 +266,18 @@ local lsp = {
                         },
                     },
                 },
+                pyright = {},
+                rust_analyzer = {},
+                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+                --
+                -- Some languages (like typescript) have entire language plugins that can be useful:
+                --    https://github.com/pmizio/typescript-tools.nvim
+                --
+                -- But for many setups, the LSP (`ts_ls`) will work just fine
+                ts_ls = {
+                    filetypes = { "js" },
+                },
+                --
             }
 
             -- Ensure the servers and tools above are installed
