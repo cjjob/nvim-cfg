ccd3f3ea380db2292c423709ea9e6167f9a596a3
Conor O'Brien <obriencjj@gmail.com>
2025-05-30 18:51:59 +0100

    Use snack's lazygit.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,9 +3,24 @@
     lazy = false,
     ---@type snacks.Config
     opts = {
+        lazygit = { enabled = true },
         terminal = { enabled = true },
     },
     keys = {
+
+        -- lazygit
+        {
+            -- You can exit by double esc'ing into normal mode and using the shortcut.
+            -- But, easier to just press q and exit process as with normal lazygit.
+            mode = "n",
+            "<leader>kl",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Open lazygit",
+        },
+
+        -- terminal
         {
             -- Set F15 to toggle float terminal.
             -- NOTE:This only makes sense because I've remapped CAPSLOCK + # to F15.
