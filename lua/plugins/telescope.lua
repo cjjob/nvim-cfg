482e22cf677bdbb3786a7488edc9ff6d71dc26fb
Conor O'Brien <obriencjj@gmail.com>
2025-06-19 18:59:29 +0100

    Telescope git changes.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -66,15 +66,20 @@ return -- Fuzzy Finder - files, lsp, etc.
 
         -- See `:help telescope.builtin`
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sc", builtin.git_commits, { desc = "[S]earch git [C]ommits" })
         vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set(
+            "n",
+            "<leader>sgc",
+            builtin.git_commits,
+            { desc = "[S]earch [G]it [C]ommits" }
+        )
+        vim.keymap.set("n", "<leader>sgs", builtin.git_status, { desc = "[S]earch [G]it [S]tatus" })
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sj", builtin.jumplist, { desc = "[S]earch [J]umplist" })
         vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
         vim.keymap.set("n", "<leader>sl", builtin.resume, { desc = "[S]earch [L]ast" })
-        vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "[S]earch [R]ecent" })
+        vim.keymap.set("n", "<leader>sr", builtin.live_grep, { desc = "[S]earch by g[R]ep" })
         vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
         vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
         vim.keymap.set("n", "<leader><leader>", function()
