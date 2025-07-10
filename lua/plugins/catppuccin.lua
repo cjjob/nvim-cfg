e215ddc0867c275076e647d9874ff1763ee99fb9
Conor O'Brien <obriencjj@gmail.com>
2025-07-10 11:01:28 +0100

    No light/dark mode.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup({
+            flavour = "frappe",
             integrations = {
                 -- https://github.com/catppuccin/nvim?tab=readme-ov-file#integrations
                 noice = true,
@@ -11,21 +12,5 @@ return {
                 which_key = true,
             },
         })
-
-        -- Set colorscheme to follow system light/dark mode.
-        local theme = "frappe"
-        if vim.fn.has("mac") == 1 then
-            local appearance = vim.fn.trim(vim.fn.system("defaults read -g AppleInterfaceStyle"))
-            if appearance == "Dark" then
-                theme = "frappe"
-            else
-                theme = "latte"
-            end
-        else
-            print("Not on macOS, using catppuccin-frappe, without following system theme.")
-            print("Linux. Did we run out of money? Windows. Are we stupid now?")
-        end
-
-        vim.cmd("colorscheme catppuccin-" .. theme)
     end,
 }
