cbfd82d2b814ef3b035ea771ce7c1118aaa79b4d
Conor O'Brien <obriencjj@gmail.com>
2025-07-25 01:51:41 +0100

    Various shit, whatever.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,24 +1,24 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--branch=stable",
-		lazyrepo,
-		lazypath,
-	})
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -37,14 +37,24 @@ vim.opt.showmode = false
 vim.opt.scrolloff = 8
 vim.opt.fillchars = { eob = " " }
 
+-- Show whitespace characters
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "»·",
+  trail = "·",
+  extends = "…",
+  precedes = "…",
+  nbsp = "␣",
+}
+
 vim.cmd("colorscheme minimal")
 require("config.keymaps")
 require("config.autocommands")
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-	},
-	install = { colorscheme = { "minimal" } },
-	checker = { enabled = false },
+    spec = {
+        { import = "plugins" },
+    },
+    install = { colorscheme = { "minimal" } },
+    checker = { enabled = false },
 })
